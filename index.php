03bb214cd6ade5b68232b70fcc242b56f8da759a
Carolina Vega <karolina.vega@gmail.com>
2015-06-04 22:46:30 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<title>La Hora del Hot Cake</title>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap -->
+    <link href="local/css/bootstrap.min.css" rel="stylesheet">
+    <link href="local/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- JQuery -->
+    <script type="text/javascript" src="local/js/jquery.js"></script>
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/
+libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+</head>
+<body> 
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                hi
+            </div>
+        </div>
+    </div>
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="local/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $('.page-container').css("height",$(window).height());
+    </script>
+</body>
+</html>
