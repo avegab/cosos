c058a13dd221b058d31abdef3a8eaa690f3cccb5
Carolina Vega <karolina.vega@gmail.com>
2015-06-07 21:12:39 -0500

    Actualizacion index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title>La Hora del Hot Cake</title>
+<title>Weather Predictor</title>
 <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
@@ -21,8 +21,22 @@ libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
     
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                hi
+            <div class="col-md-6 col-md-offset-3 center">
+                <h1 class="white center">Weather Predictor</h1>
+                <p class="lead center white">
+                    Introduce tu ciudad
+                </p>
+
+                <form>
+                    <div class="form-group">
+                        <input placeholder="Ej. Monterrey, Veracruz, Acapulco..." type="text" name="city" id="city" class="form-control"/>
+                    </div>
+                    <button id="btnWeather" class="btn btn-success btn-lg">¿Cuál es mi clima?</button>
+                </form>
+                <div id="success" class='alert alert-success'></div>
+                <div id="fail" class='alert alert-danger'>
+                    No se encontró información.
+                </div>
             </div>
         </div>
     </div>
@@ -31,7 +45,27 @@ libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="local/js/bootstrap.min.js"></script>
     <script type="text/javascript">
-        $('.page-container').css("height",$(window).height());
+
+        $('#btnWeather').click(function(e){
+            e.preventDefault();
+            if ($('#city').val()!=''){
+                $.get("scraper.php?city="+$('#city').val(),function(data){
+                    if (data!=''){
+                        $('#fail').fadeOut();
+                        $('#success').html(data).fadeIn();
+                    }else{
+                        $('#success').fadeOut();
+                        $('#fail').html('No se encontró información.');
+                        $('#fail').fadeIn();
+                    }
+                });
+            }else{
+                $('#success').fadeOut();
+                $('#fail').html('Por favor introduce una ciudad.');
+                $('#fail').fadeIn();        
+            }
+        })
+        // $('.page-container').css("height",$(window).height());
     </script>
 </body>
 </html>
